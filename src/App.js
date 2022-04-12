47effb3a2fdaffb5ac25c079492f12724786aea2
Isehou <zaku47shift@gmail.com>
2022-04-12 16:36:47 +0600

    new commit on 12.04 Workspace

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import UsestateTest from './components/someData/UsestateTest';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Header changeLink={changeLink} links={links} />
       {link === 'home' && <Home />}
       {link === 'about' && <h1>About</h1>}
+      <UsestateTest /> 
     </div>
   );
 }
---
+++ src/components/someData/UsestateTest.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function UsestateTest() {
+    const [counter, setCounter] = useState(0)
+
+    function increment() {
+        setCounter(counter + 1)
+    }
+    function decrement() {
+        setCounter(counter - 1)
+    }
+    return (
+        <div className="someDiv">
+        <h1>Счетчик: {counter}</h1>
+        <button onClick={increment} className='SomeButton_acces'>Добавить</button>
+        <button onClick={decrement} className='SomeButton_dange'>Убавить</button>
+        </div>
+    )
+}
+
+export default UsestateTest
