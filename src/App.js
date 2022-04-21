80bcab6302e43c4f5c1006481c9f1cfb82f5a3ab
Isehou <zaku47shift@gmail.com>
2022-04-21 16:55:06 +0600

    21.04

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import UseStateTest from './components/someData/UsestateTest';
 import UseEffectTest from './components/someData/UseEffectTest';
+import { Routes,  Route, } from "react-router-dom";
 
 
 function App() {
@@ -17,8 +18,12 @@ function App() {
       <Header changeLink={changeLink} links={links} />
       {link === 'home' && <Home />}
       {link === 'about' && <h1>About</h1>}
-      <UseStateTest /> 
-      <UseEffectTest /> 
+
+        <Routes>
+            <Route path="/components/someData/UseStateTest" element={ <UseStateTest /> }></Route>
+            <Route path="/components/someData/UseEffectTest" element={ <UseEffectTest /> }>{}</Route>
+        </Routes>
+
     </div>
   );
 }
