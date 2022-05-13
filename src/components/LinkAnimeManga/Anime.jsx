fb499de2d9aa6cbe5cdb4c4e7458f89791994f4f
Isehou <zaku47shift@gmail.com>
2022-05-13 15:58:04 +0600

    new commit 13.05

--- src/components/LinkAnimeManga/Anime.jsx
+++ src/components/LinkAnimeManga/Anime.jsx
@@ -3,41 +3,39 @@ import "./style.css";
 import { useParams } from "react-router-dom";
 
 function Anime(props) {
-  const [animeData, setAnimeData] = useState(null);
-  const [animeDetail, setAnimeDetail] = useState();
+  const [aniMangaData, setAniMangaData] = useState(null);
   let params = useParams();
   useEffect(() => {
     fetch(`https://shikimori.one/api/animes/${params.id}`)
       .then((res) => res.json())
-      .then((res) => setAnimeData(res))
+      .then((res) => setAniMangaData(res));
   }, [params]);
   return (
-    <div className="animes_main">
-      <div className="animes_main_details">
-        {/* <pre>{JSON.stringify(params, null, 5)}</pre> */}
-
-        <h2>
-          {animeData ? animeData.name : ""} /
-          {animeData ? animeData.russian : ""}
-        </h2>
-        <h4>
-          {animeData ? animeData.english : ""} / 
-          {animeData ? animeData.japanese : ""}
-        </h4>
-        <img 
-          className="animes_image_block" 
-          src={"animeData.image.original"} alt="#">
-        </img>
-        
-        <p>Type: {animeData ? animeData.kind : ""}</p>
-        <p>Rating: {animeData ? animeData.score : ""}</p>
-        <p>Aired on: {animeData ? animeData.aired_on : ""}</p>
-        <p>released_on: {animeData ? animeData.released_on : ""}</p>
-        <p>Episode: {animeData ? animeData.episodes : ""}</p>
-        <p>Status: {animeData ? animeData.status : ""}</p>
+    <div className="aniManga-main">
+      {aniMangaData && (
+        <div className="aniManga-main_details">
+          <h2>
+            {aniMangaData.name} / {aniMangaData.russian}
+          </h2>
+          <h3>
+            {aniMangaData.english} / {aniMangaData.japanese}
+          </h3>
+          {aniMangaData?.image?.original && (
+            <img
+              className="aniManga-image_block"
+              src={"https://shikimori.one" + aniMangaData.image.original}
+              alt="#"
+            ></img>
+          )}
+          <p>Тип: {aniMangaData.kind}</p>
+          <p>Рейтинг: {aniMangaData.score}</p>
+          <p>Aired on: {aniMangaData.aired_on}</p>
+          <p>released_on: {aniMangaData.released_on}</p>
+          <p>Episode: {aniMangaData.episodes}</p>
+          <p>Status: {aniMangaData.status}</p>
         </div>
+      )}
     </div>
-    
   );
 }
 
