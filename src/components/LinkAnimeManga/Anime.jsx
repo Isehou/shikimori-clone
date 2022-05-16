4c92bf81e81702a1e691ddbbf102f8fd2a0a57ba
Isehou Mateno <mateno47@gmail.com>
2022-05-16 23:38:15 +0600

    night 09.05

--- src/components/LinkAnimeManga/Anime.jsx
+++ src/components/LinkAnimeManga/Anime.jsx
@@ -4,40 +4,38 @@ import { useParams } from "react-router-dom";
 
 function Anime(props) {
   const [animeData, setAnimeData] = useState(null);
-  const [animeDetail, setAnimeDetail] = useState();
   let params = useParams();
   useEffect(() => {
     fetch(`https://shikimori.one/api/animes/${params.id}`)
       .then((res) => res.json())
-      .then((res) => setAnimeData(res))
+      .then((res) => setAnimeData(res));
   }, [params]);
   return (
     <div className="animes_main">
-      <div className="animes_main_details">
-        {/* <pre>{JSON.stringify(params, null, 5)}</pre> */}
-
-        <h2>
-          {animeData ? animeData.name : ""} /
-          {animeData ? animeData.russian : ""}
-        </h2>
-        <h4>
-          {animeData ? animeData.english : ""} / 
-          {animeData ? animeData.japanese : ""}
-        </h4>
-        <img 
-          className="animes_image_block" 
-          src={"animeData.image.original"} alt="#">
-        </img>
-        
-        <p>Type: {animeData ? animeData.kind : ""}</p>
-        <p>Rating: {animeData ? animeData.score : ""}</p>
-        <p>Aired on: {animeData ? animeData.aired_on : ""}</p>
-        <p>released_on: {animeData ? animeData.released_on : ""}</p>
-        <p>Episode: {animeData ? animeData.episodes : ""}</p>
-        <p>Status: {animeData ? animeData.status : ""}</p>
+      {animeData && (
+        <div className="animes_main_details">
+          <h2>
+            {animeData.name} / {animeData.russian}
+          </h2>
+          <h3>
+            {animeData.english} / {animeData.japanese}
+          </h3>
+          {animeData?.image?.original && (
+            <img
+              className="animes_image_block"
+              src={"https://shikimori.one" + animeData.image.original}
+              alt="#"
+            ></img>
+          )}
+          <p>Тип: {animeData.kind}</p>
+          <p>Рейтинг: {animeData.score}</p>
+          <p>Aired on: {animeData.aired_on}</p>
+          <p>released_on: {animeData.released_on}</p>
+          <p>Episode: {animeData.episodes}</p>
+          <p>Status: {animeData.status}</p>
         </div>
+      )}
     </div>
-    
   );
 }
 
