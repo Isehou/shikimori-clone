6ca1390898ede272c0f3d9dab8f7e92c9fdd3c0d
Isehou <zaku47shift@gmail.com>
2022-05-31 16:36:25 +0600

    office 31.05

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,21 +6,26 @@ import { SiShikimori } from "react-icons/si";
 const Header = (props) => {
   return (
     <div className="header">
-      <div className="head_main-text">
+      <div className="container">
         <SiShikimori className="head_shiki_logo"></SiShikimori>
-        Shikimori clone
+        <div className="head_title-text">
+          Shikimori clone
+        </div>
+        <div className="header_other">
+          <span></span>
+        </div>
+        <ul className="head_list">
+          {props.links.map((el, i) => {
+            return (
+              <li key={i}>
+                <Link className="header_link" to={el.link}>
+                  {el.label}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-      <ul className="head_list">
-        {props.links.map((el, i) => {
-          return (
-            <li key={i}>
-              <Link className="header_link" to={el.link}>
-                {el.label}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
     </div>
   );
 };
