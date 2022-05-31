4fadeebd796c875a0dfec826c2b7263993b190ab
Isehou Mateno <mateno47@gmail.com>
2022-05-31 21:45:00 +0600

    home 31.05

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,14 +7,12 @@ const Header = (props) => {
   return (
     <div className="header">
       <div className="container">
-        <SiShikimori className="head_shiki_logo"></SiShikimori>
-        <div className="head_title-text">
-          Shikimori clone
-        </div>
-        <div className="header_other">
+        <SiShikimori className="head__shiki__logo"></SiShikimori>
+        <div className="head__title__text">Shikimori clone</div>
+        <div className="head__subitle__other">
           <span></span>
         </div>
-        <ul className="head_list">
+        <ul className="head__list">
           {props.links.map((el, i) => {
             return (
               <li key={i}>
