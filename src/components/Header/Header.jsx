3009a34f25e48832b45ef433a2a85fc6bf2485ce
Isehou <zaku47shift@gmail.com>
2022-06-01 15:08:25 +0600

    office 01.06

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,17 +12,19 @@ const Header = (props) => {
         <div className="head__subitle__other">
           <span></span>
         </div>
-        <ul className="head__list">
-          {props.links.map((el, i) => {
-            return (
-              <li key={i}>
-                <Link className="header_link" to={el.link}>
-                  {el.label}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+        <div className="head__menu">
+          <ul className="head__list">
+            {props.links.map((el, i) => {
+              return (
+                <li key={i}>
+                  <Link className="header__link" to={el.link}>
+                    {el.label}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
