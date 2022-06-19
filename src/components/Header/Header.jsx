103dd27f4eb52d75598d4ae78f965e7a7fb020ca
Isehou Mateno <mateno47@gmail.com>
2022-06-20 00:03:42 +0600

    home 19.06

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,23 +2,27 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import { SiShikimori } from "react-icons/si";
-import HandleClick from "./HandleClick";
+import { useState } from "react";
 
 function Header(props) {
+  const [isOpen, setIsOpen] = useState();
+  const handleClick = () => {
+    setIsOpen((prev) => !prev);
+  };
   return (
     <div className="header">
-      <div className="container" id="container">
+      <div className="container">
         <SiShikimori className="head__shiki__logo"></SiShikimori>
         <div className="head__title__text">Shikimori clone</div>
         <button
           className="head__adaptive__btn"
           id="head__adaptive__btn"
-          onClick={HandleClick}
+          onClick={handleClick}
         >
           <span></span>
         </button>
-        <div className="head__menu" id="head__menu">
-          <ul className="head__list" onClick={HandleClick}>
+        <div className={`head__menu ${isOpen && "active"}`}>
+          <ul className="head__list" onClick={handleClick}>
             {props.links.map((el, i) => {
               return (
                 <li key={i}>
