3b04ccd1fbe133cb51d61b477051fbb61265bba0
Isehou <zaku47shift@gmail.com>
2022-08-05 16:55:07 +0600

    office 05.08

---
+++ src/components/MainAnimeManga/Filter/FilterFunction.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./FilterFunction.css";
+
+function FilterFunction({ filterList, toggleFilter }) {
+  const [itemsFromData, setItemsFromData] = useState([]);
+  useEffect(() => {
+    fetch("https://shikimori.one/api/genres")
+      .then((res) => res.json())
+      .then((res) => setItemsFromData(res.filter((e) => e.kind === "anime")));
+  }, []);
+
+  return (
+    <div className="filter">
+      <div className="filter__block">
+        <ul className="filter__items">
+          <div className="filter__item-genre">ЖАНРЫ</div>
+          {itemsFromData.map((el, i) => (
+            <li
+              className={`filter__item ${filterList[el.id] ? "active" : ""}`}
+              key={el.id}
+            >
+              <input
+                autoComplete="off"
+                type="checkbox"
+                onClick={() => toggleFilter(el.id)}
+              />
+              {el.russian}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default FilterFunction;
--- src/components/MainAnimeManga/SortAndFilter/Sort.jsx
+++ src/components/MainAnimeManga/SortAndFilter/Sort.jsx
@@ -1,10 +1,17 @@
 import "./Filter__Sort.css";
+import { React, useEffect, useState } from "react";
 
 export default function Sort({ value }) {
+  const [sortItems, setSortItems] = useState();
   console.log(value);
   return (
-    <div className="container">
-      <div className="sort__main"></div>
+    <div className="sort">
+      <div className="sort__block">
+        <div className="sort__items">
+          <div className="sort__item-genre">СОРТИРОВКА</div>
+          <div className="sort__item"></div>
+        </div>
+      </div>
     </div>
   );
 }
