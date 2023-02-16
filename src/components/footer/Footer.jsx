76371a3438067d9319970295e13c94a07ddd05ec
isehou <zaku47shift@gmail.com>
2023-02-16 12:20:00 +0600

    delete tailwind and small fixes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,33 @@
 import React from "react";
-import "./footer.css";
+import styled from "styled-components";
+
+const StyledFooter = styled.div`
+  box-shadow: var(--shadow);
+  width: 100%;
+  height: 55px;
+  display: flex;
+  background-color: #173a5e;
+  align-items: center;
+  color: white;
+  font-family: "Trebuchet MS", tahoma, arial, verdana;
+  font-size: 12pt;
+  justify-content: flex-end;
+`;
+
+const StyledCopyright = styled.div`
+  padding-right: 100px;
+  display: flex;
+  justify-content: flex-end;
+  color: white;
+`;
 
 const Footer = () => {
   return (
-    <div className="footer">
-      {/* <div className="adress">ETERNITY</div> */}
-      <div className="copyright">
+    <StyledFooter>
+      <StyledCopyright>
         SHIKIMORI CLONE. Project start: March 2022
-      </div>
-    </div>
+      </StyledCopyright>
+    </StyledFooter>
   );
 };
 
